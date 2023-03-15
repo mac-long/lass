8f6cc318d1f7f59cdb1bf38c2d6c24ee9510805b
Mac <mac@3sidedcube.com>
2023-03-15 15:46:27 +0000

    fix(components): updated import paths

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	import '../app.postcss';
 	import '@fontsource/poppins/700.css';
 	import { configVisibile, teamsTable } from '$lib/store';
-	import Header from '$lib/components/Header.svelte';
-	import Table from '$lib/components/table.svelte';
-	import Config from '$lib/components/config.svelte';
+	import Header from '../lib/components/Header.svelte';
+	import Table from '../lib/components/table.svelte';
+	import Config from '../lib/components/config.svelte';
 
 	export let data;
 
