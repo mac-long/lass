fc2401900a24934851f8231069fb32339ecdb6fa
Mac <115668288+mac-long@users.noreply.github.com>
2023-03-16 10:20:59 +0000

    fix(header): Added aria label

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -23,6 +23,6 @@
 	<h1 class="text-4xl font-bold">3 Sided Cube FC League</h1>
 </div>
 
-<button class="absolute top-0 right-0 p-4 text-black dark:text-white hover:text-primary-light dark:hover:text-primary-dark duration-300" on:click={() => configVisibile.set(true)}>
+<button class="absolute top-0 right-0 p-4 text-black dark:text-white hover:text-primary-light dark:hover:text-primary-dark duration-300" on:click={() => configVisibile.set(true)} aria-label="New Fixture">
 	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
 </button>
