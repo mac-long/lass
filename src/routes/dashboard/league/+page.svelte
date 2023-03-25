f7c574c565e5902e3b165c42f14ebbde3860aadd
Mac <mac@3sidedcube.com>
2023-03-25 13:17:43 +0000

    chore: director structure

---
+++ src/routes/dashboard/league/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.postcss';
+	import '@fontsource/poppins/700.css';
+	import { configVisibile, teamsTable } from '$lib/store';
+	import Header from '$lib/components/header.svelte';
+	import Table from '$lib/components/table.svelte';
+	import Config from '$lib/components/config.svelte';
+
+	export let data;
+
+	teamsTable.set(data.teams.sort((a, b) => b.pts - a.pts));
+</script>
+
+<main>
+	<div class={`${$configVisibile ? 'opacity-50 pointer-events-none' : ''}`}>
+		<Header />
+		<Table />
+	</div>
+
+	<Config />
+</main>
