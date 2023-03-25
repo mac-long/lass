b2e202ed303ae34e101ca5ee6d09b55e661fab7a
Mac <mac@3sidedcube.com>
2023-03-25 17:02:19 +0000

    feat(db): added teams and leagues to db

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1 class="text-2xl font-bold mb-4">Welcome</h1>
 <p>League Tables is an app for creating league tables for any sport</p>
+<p>Currently it utilises football's scoring system, but if wanted I will work on adding more sports</p>
