0bb5cf3d5872d770efe733d94bb300fafdfecb0d
Mac <mac@3sidedcube.com>
2023-03-25 21:29:14 +0000

    feat(league): data now fetching.

---
+++ src/routes/dashboard/[league]/+page.server.js
@@ -0,0 +1,25 @@
+import { supabase } from '$lib/supabase';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals: { getSession }, params }) {
+	const session = await getSession();
+
+	if (!session) {
+		throw error(401, 'Not logged in');
+	}
+
+	const { data: league, error } = await supabase
+		.from('leagues')
+		.select('*')
+		.eq('id', params.league);
+
+	if (error) {
+		throw error(500, 'Error loading league');
+	}
+
+	if (league) {
+		return {
+			league
+		};
+	}
+}
