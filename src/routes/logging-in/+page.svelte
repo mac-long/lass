970a20ae01d23394dfa90495ca891cbbf0f1e284
Mac <mac@3sidedcube.com>
2023-03-25 15:44:48 +0000

    chore: added loading animation text

--- src/routes/logging-in/+page.svelte
+++ src/routes/logging-in/+page.svelte
@@ -13,8 +13,4 @@
 	}
 </script>
 
-<section>
-	"Because as we know, there are known knowns; there are things we know we know. We also know there
-	are known unknowns; that is to say we know there are some things we do not know. But there are
-	also unknown unknowns—the ones we don't know we don't know" - Donald Rumsfeld
-</section>
+<h1>Add Loading Animation Here</h1>
