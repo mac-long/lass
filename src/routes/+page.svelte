4e3bcc72b04a1b889ba32853d3d832574239ae9b
Mac <115668288+mac-long@users.noreply.github.com>
2023-03-26 21:52:14 +0100

    fix(/): gh url corrected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,6 @@
 	description="Lass (Leagues as a Service) is a platform for creating and managing league tables. Currently utilises the football league scoring method, possible other sports/games to be added later."
 />
 <div class="flex justify-center items-center space-x-2">
-  <a class="button secondary" href="https:/:github.com/mac-long/league-tables">Contribute</a>
+  <a class="button secondary" href="https://github.com/mac-long/league-tables" target="_blank">Contribute</a>
   <a class="button" href="/get-started">Get Started</a>
 </div>
