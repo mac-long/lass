8c5c52f8f868425d3feb6aa7020a04d1356f2727
Mac <115668288+mac-long@users.noreply.github.com>
2023-03-26 21:48:44 +0100

    feat(/): added buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,9 @@
 
 <Header
 	title="Welcome to Lass"
-	description="Lass (Leagues as a Service) is a platform for creating and managing league tables."
+	description="Lass (Leagues as a Service) is a platform for creating and managing league tables. Currently utilises the football league scoring method, possible other sports/games to be added later."
 />
+<div class="flex items-center space-x-2">
+  <a class="secondary" href="https:/:github.com/mac-long/league-tables">Contribute</a>
+  <a href="/get-started">Get Started</a>
+</div>
