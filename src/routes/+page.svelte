f18ef7e438605bb5b406114711727352bc69ce51
Mac <mac@3sidedcube.com>
2023-03-27 08:45:17 +0100

    fix(/): removed testing code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-	import Container from '$lib/forms/container.svelte';
-	import Input from '$lib/forms/input.svelte';
 	import Header from '$lib/general/header.svelte';
-	import Edit from '$lib/icons/edit.svelte';
 	import { writable } from 'svelte/store';
 	const teams = [];
 	const open = writable(true);
@@ -14,8 +11,6 @@
 	description="Lass (Leagues as a Service) is a platform for creating and managing league tables."
 />
 
-<Container title="Teams" {open} lg>
-</Container>
 <div class="flex justify-center items-center space-x-2">
 	<a class="button secondary" href="https://github.com/mac-long/league-tables" target="_blank"
 		>Contribute</a
